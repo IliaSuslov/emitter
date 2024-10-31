ce426f3f7d1f384f4fbfcb245bd7d7c6f3865415
isuslov <isuslov@me.com>
2024-10-31 15:46:49 +0300

    first commit

--- emitter-sync-pattern.ts
+++ emitter-sync-pattern.ts
@@ -1,7 +1,7 @@
 /* Check the comments first */
 
 import { EventEmitter } from "./emitter";
-import { EventDelayedRepository } from "./event-repository";
+import { EVENT_SAVE_DELAY_MS, EventDelayedRepository, EventRepositoryError } from "./event-repository";
 import { EventStatistics } from "./event-statistics";
 import { ResultsTester } from "./results-tester";
 import { triggerRandomly } from "./utils";
@@ -37,6 +37,7 @@ function init() {
     repository,
   });
   resultsTester.showStats(20);
+
 }
 
 /* Please do not change the code above this line */
@@ -59,20 +60,41 @@ class EventHandler extends EventStatistics<EventName> {
     super();
     this.repository = repository;
 
-    emitter.subscribe(EventName.EventA, () =>
-      this.repository.saveEventData(EventName.EventA, 1)
-    );
+    EventName: [EventName.EventA, EventName.EventB].forEach(eventName =>
+      emitter.subscribe(eventName, () => this.saveEventData(eventName, 1)))
+  }
+
+  saveEventData(eventName: EventName, i: number) {
+    this.repository.saveEventData(eventName, i);
+    this.setStats(eventName, (this.getStats(eventName) || 0) + 1);
   }
 }
 
 class EventRepository extends EventDelayedRepository<EventName> {
   // Feel free to edit this class
+  private last: number = 0;
+  private cacheEvent: Map<EventName, number> = new Map();
 
-  async saveEventData(eventName: EventName, _: number) {
+  async saveEventData(eventName: EventName, i: number) {
+
+    const acum = (this.cacheEvent.get(eventName) || 0) + i;
+
+    const now = new Date().getTime();
+    if (now < this.last + EVENT_SAVE_DELAY_MS) {
+      this.cacheEvent.set(eventName, acum);
+      return
+    }
+    this.last = now;
+    this.cacheEvent.set(eventName, 0);
+
     try {
-      await this.updateEventStatsBy(eventName, 1);
-      this;
-    } catch (e) {
+      await this.updateEventStatsBy(eventName, acum);
+    } catch (err) {
+      switch (err) {
+        case EventRepositoryError.TOO_MANY: this.saveEventData(eventName, acum);
+        case EventRepositoryError.REQUEST_FAIL: this.saveEventData(eventName, acum);
+        case EventRepositoryError.RESPONSE_FAIL:
+      }
       // const _error = e as EventRepositoryError;
       // console.warn(error);
     }
